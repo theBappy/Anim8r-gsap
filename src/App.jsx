7e52bb7d11267d3d7874ea079b77ce87f842eb40
theBappy <abappy575@gmail.com>
2025-06-27 00:07:04 +0600

    repo initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src="/images/nav-logo.svg" alt="logo" />
+    </nav>
+  )
+}
+
+export default Navbar
