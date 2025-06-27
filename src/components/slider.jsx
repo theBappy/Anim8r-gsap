1ce75d82cb1fb43b6579b5847c9210c80a96c143
theBappy <abappy575@gmail.com>
2025-06-27 21:22:04 +0600

    slider ui for the different items of showcase with gsap splitTrigger and SplitText plugin

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,7 +1,21 @@
+import { flavorlists } from "../constants"
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {
+          flavorlists.map((flavor) => ( 
+          <div className="relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none" key={flavor.name}>
+              <img src={`/images/${flavor.color}-bg.svg`} alt="flavor-bg" className="absolute bottom-0" />
+              <img src={`/images/${flavor.color}-drink.webp`} alt="flavor-elements" className="drinks" />
+              <img className="elements" src={`/images/${flavor.color}-elements.webp`} alt="floating-img" />
+              <h1>{flavor.name}</h1>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
